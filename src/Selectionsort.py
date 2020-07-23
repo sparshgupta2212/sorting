cb10cc87a615dd58fe86aa0c21a9c1a0b58bd298
sparshgupta2212 <sparsh.gupta_cs18@gla.ac.in>
2020-07-23 17:55:46 +0530

    selection sort

---
+++ src/Selectionsort.py
@@ -0,0 +1,18 @@
+def selectionSort(array):
+    n = len(array)
+    for i in range(n):
+
+        minimum = i
+
+        for j in range(i + 1, n):
+            if (array[j] < array[minimum]):
+                minimum = j
+        temp = array[i]
+        array[i] = array[minimum]
+        array[minimum] = temp
+
+    return array
+
+
+array = [99, 55, 29, 1, 56, 35, 45, 75, 89, 52]
+print(selectionSort(array))
